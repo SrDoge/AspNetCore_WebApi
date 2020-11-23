3d15c54d8b112ba0e33a807990500d861691eafb
julio.dias <julio.dias@DT-ECO-24>
2020-11-23 10:14:10 -0300

    Atualização na classe DAL

--- AspNetCore_WebApi/Models/Funcionario.cs
+++ AspNetCore_WebApi/Models/Funcionario.cs
@@ -5,22 +5,33 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ApnCore_CrudExemplo.Models
+namespace AspNetCore_WebApi.Models
 {
-    [Table("Funcionarios")]
+    [Table("crud_Funcionarios")]
     public class Funcionario
     {
         [Key]
+        [Display(Name = "Id")]
         public int FuncionarioId { get; set; }
 
         [Required]
+        [Display(Name = "Nome do Cliente")]
         public string Nome { get; set; }
 
         [Required]
+        [Display(Name = "Nome da Cidade")]
         public string Cidade { get; set; }
+
+        [Display(Name = "Nome do Departamento")]
         public string Departamento { get; set; }
 
         [Required]
+        [Display(Name = "Sexo")]
         public string Sexo { get; set; }
+
+        [Required]
+        [Display(Name = "Salario")]
+        public float Salario { get; set; }
     }
 }
+
